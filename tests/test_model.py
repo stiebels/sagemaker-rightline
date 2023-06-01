47f13e8cb179c2e43a03e025f83a40848fd9b98d
stiebels <15859432+stiebels@users.noreply.github.com>
2023-06-01 21:29:53 +0200

    refactor usage of validation name (#23)

--- tests/test_model.py
+++ tests/test_model.py
@@ -42,18 +42,27 @@ def test_configuration_validate_input_validations(validations, error: bool) -> N
     [
         [
             ValidationResult(
-                success=True, negative=False, message="test-message", subject="test-subject-1"
+                success=True,
+                negative=False,
+                message="test-message",
+                subject="test-subject-1",
+                validation_name="test",
             )
         ],
         [
             ValidationResult(
-                success=True, negative=False, message="test-message-true", subject="test-subject-2"
+                success=True,
+                negative=False,
+                message="test-message-true",
+                subject="test-subject-2",
+                validation_name="test",
             ),
             ValidationResult(
                 success=False,
                 negative=False,
                 message="test-message-false",
                 subject="test-subject-3",
+                validation_name="test",
             ),
         ],
     ],
@@ -64,13 +73,6 @@ def test_configuration_make_report(results) -> None:
     assert isinstance(report, Report)
     assert isinstance(report.results, list)
     assert isinstance(report.results[0], ValidationResult)
-
-
-def flatten_report(report: Report) -> list:
-    flat_list = []
-    for result in report.results:
-        flat_list += result
-    return flat_list
 
 
 @mock_ecr
@@ -91,8 +93,7 @@ def test_configuration_run(sagemaker_pipeline, ecr_client, report_length, valida
         cf = Configuration(validations=validations, sagemaker_pipeline=sagemaker_pipeline)
         report = cf.run(fail_fast=False)
 
-    report_flat = flatten_report(report=report)
-    assert len(report_flat) == report_length
+    assert len(report.results) == report_length
 
 
 @mock_ecr
@@ -107,8 +108,7 @@ def test_configuration_run_fail_fast(sagemaker_pipeline, report_length, validati
     """Test run method of Configuration class."""
     cf = Configuration(validations=validations, sagemaker_pipeline=sagemaker_pipeline)
     report = cf.run(fail_fast=True)
-    report_flat = flatten_report(report=report)
-    assert len(report_flat) == report_length
+    assert len(report.results) == report_length
 
 
 def test_report_to_df() -> None:
@@ -116,16 +116,18 @@ def test_report_to_df() -> None:
     validation_name = "StepImagesExistOnEcr"
     report = Report(
         results=[
-            {
-                validation_name: ValidationResult(
-                    success=True, negative=False, message="test-message-0", subject="test-subject-0"
-                ),
-            }
+            ValidationResult(
+                success=True,
+                negative=False,
+                message="test-message-0",
+                subject="test-subject-0",
+                validation_name=validation_name,
+            ),
         ]
     )
     df = report.to_df()
     assert df.shape == (1, 5)
-    assert df.columns.tolist() == ["validation_name", "negative", "subject", "success", "message"]
+    assert df.columns.tolist() == list(ValidationResult.__annotations__.keys())
     assert df["success"].tolist() == [True]
     assert df["negative"].tolist() == [False]
     assert df["message"].tolist() == ["test-message-0"]
