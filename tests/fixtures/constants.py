dc4e359502086b98858f04df96dc6f70548631d4
sist <15859432+stiebels@users.noreply.github.com>
2023-06-04 18:33:22 +0200

    Added role steps

--- tests/fixtures/constants.py
+++ tests/fixtures/constants.py
@@ -1,6 +1,7 @@
 from moto.core import DEFAULT_ACCOUNT_ID as TEST_ACCOUNT_ID
 
-TEST_ROLE_ARN = f"arn:aws:iam::{TEST_ACCOUNT_ID}:role/TestRole"
+TEST_ROLE_NAME = "TestRole"
+TEST_ROLE_ARN = f"arn:aws:iam::{TEST_ACCOUNT_ID}:role/{TEST_ROLE_NAME}"
 TEST_REGION_NAME = "eu-west-1"
 TEST_AWS_ACCESS_KEY_ID = "access_key"
 TEST_AWS_SECRET_ACCESS_KEY = "secret_key"
--- tests/utils.py
+++ tests/utils.py
@@ -5,7 +5,7 @@ import boto3
 import pytest
 
 from sagemaker_rightline.validations import ContainerImage
-from tests.fixtures.constants import TEST_REGION_NAME, TEST_ROLE_ARN
+from tests.fixtures.constants import TEST_REGION_NAME, TEST_ROLE_ARN, TEST_ROLE_NAME
 from tests.fixtures.image_details import IMAGE_MANIFEST
 
 
@@ -19,30 +19,40 @@ def lambda_client():
     return boto3.client("lambda", region_name=TEST_REGION_NAME)
 
 
-def get_iam_client():
+@pytest.fixture(autouse=False)
+def iam_client():
     return boto3.client("iam", region_name=TEST_REGION_NAME)
 
 
 @contextmanager
-def create_lambda_function(lambda_client, function_names: List[str]) -> None:
-    get_iam_client().create_role(
-        RoleName=TEST_ROLE_ARN.split("/")[-1],
-        AssumeRolePolicyDocument='{"Version": "2012-10-17","Statement": '
-        '[{"Effect": "Allow","Principal": {"Service": '
-        '"lambda.amazonaws.com"},"Action": '
-        '"sts:AssumeRole"}]}',
-    )
-    for function_name in function_names:
-        lambda_client.create_function(
-            FunctionName=function_name,
-            Runtime="python3.8",
-            Role=TEST_ROLE_ARN,
-            Handler="lambda_function.lambda_handler",
-            Code={"ZipFile": "lambda code"},
+def create_iam_role(iam_client, role_names: List[str]) -> None:
+    for role_name in role_names:
+        iam_client.create_role(
+            RoleName=role_name,
+            AssumeRolePolicyDocument='{"Version": "2012-10-17","Statement": '
+            '[{"Effect": "Allow","Principal": {"Service": '
+            '"lambda.amazonaws.com"},"Action": '
+            '"sts:AssumeRole"}]}',
         )
     yield
-    for function_name in function_names:
-        lambda_client.delete_function(FunctionName=function_name)
+    for role_name in role_names:
+        iam_client.delete_role(RoleName=role_name)
+
+
+@contextmanager
+def create_lambda_function(lambda_client, iam_client, function_names: List[str]) -> None:
+    with create_iam_role(iam_client, [TEST_ROLE_NAME]):
+        for function_name in function_names:
+            lambda_client.create_function(
+                FunctionName=function_name,
+                Runtime="python3.8",
+                Role=TEST_ROLE_ARN,
+                Handler="lambda_function.lambda_handler",
+                Code={"ZipFile": "lambda code"},
+            )
+        yield
+        for function_name in function_names:
+            lambda_client.delete_function(FunctionName=function_name)
 
 
 @contextmanager
