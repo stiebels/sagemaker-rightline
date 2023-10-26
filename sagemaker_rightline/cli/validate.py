c15689ca5360e0b80faf75d468a025470cfbe71c
Simon Stiebellehner <stiebels@simons-air.kpn>
2023-10-26 21:31:08 +0200

    add test for workdir change

--- sagemaker_rightline/cli/validate.py
+++ sagemaker_rightline/cli/validate.py
@@ -2,7 +2,6 @@ import argparse
 import importlib.util
 import logging
 import os
-from os import path
 
 
 def parse_args() -> argparse.Namespace:
@@ -31,10 +30,12 @@ def parse_args() -> argparse.Namespace:
 def main():
     args = parse_args()
 
-    if args.working_dir:
-        os.chdir(args.working_dir)
+    try:
+        os.chdir(os.path.abspath(args.working_dir))
+    except AttributeError:
+        pass
 
-    abs_path = path.abspath(args.configuration)
+    abs_path = os.path.abspath(args.configuration)
     spec = importlib.util.spec_from_file_location("get_configuration", abs_path)
     sm_rightline_config_module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(sm_rightline_config_module)
--- tests/test_cli_validate.py
+++ tests/test_cli_validate.py
@@ -1,3 +1,4 @@
+import os
 from argparse import Namespace
 from unittest.mock import patch
 
@@ -5,6 +6,27 @@ import pytest
 
 from sagemaker_rightline.cli.validate import main
 from sagemaker_rightline.model import Report, ValidationResult
+
+
+@patch("sagemaker_rightline.cli.validate.parse_args")
+def test_main_working_dir_change(parse_args) -> None:
+    """Positive test for CLI validate command."""
+    old_working_dir = os.getcwd()
+    new_working_dir = "tests/fixtures"
+
+    parse_args.return_value = Namespace(
+        configuration="tests/fixtures/cli_input.py",
+        working_dir=new_working_dir,
+    )
+    with pytest.raises(FileNotFoundError):
+        main()
+
+    try:
+        assert os.getcwd().endswith(new_working_dir)
+    except AssertionError:
+        raise
+    finally:
+        os.chdir(old_working_dir)
 
 
 @patch("sagemaker_rightline.cli.validate.parse_args")
